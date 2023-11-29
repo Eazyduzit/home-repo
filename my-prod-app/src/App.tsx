7585a0a61d739dd96846cc4d09da0aad8cb66369
Marius Skjellerud <m_skjellerud@hotmail.com>
2023-11-29 15:19:48 +0100

    added ProfileCard component

--- my-prod-app/src/App.tsx
+++ my-prod-app/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Avatar from './Avatar'
 import Links from './Links'
+import { ProfileCard } from './ProfileCard'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <h1>Hei, mitt navn er Marius!</h1>
       <h2>Jeg er en React og Typescript utvikler, i frontend..</h2>
       <Links />
+      <ProfileCard />
     </div>
   )
 }
---
+++ my-prod-app/src/ProfileCard.tsx
@@ -0,0 +1,3 @@
+export const ProfileCard = () => {
+  return <div>Hola!</div>
+}
