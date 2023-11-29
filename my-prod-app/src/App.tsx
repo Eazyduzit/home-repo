f030e5a203d9818a1b3efe316ed1017d9e5a482b
Marius Skjellerud <m_skjellerud@hotmail.com>
2023-11-29 15:05:33 +0100

    renamed file

--- my-prod-app/src/App.tsx
+++ my-prod-app/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import Avatar from './Avatar'
-// import List from './List'
+import Links from './Links'
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <Avatar />
       <h1>Hei, mitt navn er Marius!</h1>
       <h2>Jeg er en React og Typescript utvikler, i frontend..</h2>
-      {/* <List /> */}
+      <Links />
     </div>
   )
 }
---
+++ my-prod-app/src/Links.tsx
@@ -0,0 +1,16 @@
+function Links() {
+  return (
+    <div>
+      <ul>
+        <li>List of components..</li>
+        <li>
+          <a href="">Test component</a>
+        </li>
+        <li>
+          <a href="">Another link..</a>
+        </li>
+      </ul>
+    </div>
+  )
+}
+export default Links
