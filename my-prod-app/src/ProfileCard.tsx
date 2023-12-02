75464e9b7b07123a3b00ca8c53dd0620ef2659c1
Marius Skjellerud <m_skjellerud@hotmail.com>
2023-12-02 13:12:38 +0100

    installed clsx

--- my-prod-app/src/ProfileCard.tsx
+++ my-prod-app/src/ProfileCard.tsx
@@ -1,3 +1,15 @@
+import { Paragraph, ScreenReaderOnly } from '@sikt/sds-core'
+// import clsx from 'cslx'
+
 export const ProfileCard = () => {
-  return <div>Hola!</div>
+  return (
+    <div className="test">
+      <div className="cardContent">
+        <Paragraph as="span" variant="regular" modifier="strong">
+          Test
+        </Paragraph>
+        <ScreenReaderOnly>Test</ScreenReaderOnly>
+      </div>
+    </div>
+  )
 }
