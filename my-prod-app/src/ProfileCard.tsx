70cdd7f9435d726fd48f8ea60225a5972ecb9c6d
Marius Skjellerud <m_skjellerud@hotmail.com>
2023-12-02 13:21:40 +0100

    added props

--- my-prod-app/src/ProfileCard.tsx
+++ my-prod-app/src/ProfileCard.tsx
@@ -1,9 +1,18 @@
 import { Paragraph, ScreenReaderOnly } from '@sikt/sds-core'
+import { HTMLAttributes, ReactNode } from 'react'
 // import clsx from 'cslx'
 
 type ProfileCardVariant = 'success' | 'warning' | 'critical'
 
-export const ProfileCard = ({ variant, children, ...rest }) => {
+export interface ProfileCardProps extends HTMLAttributes<HTMLDivElement> {
+  children: ReactNode
+  variant: ProfileCardVariant
+}
+
+export const ProfileCard = ({ variant, children, ...rest }: ProfileCardProps) => {
+  //   let screenReader
+  //   let Icon
+
   return (
     <div className="test">
       <div className="cardContent">
