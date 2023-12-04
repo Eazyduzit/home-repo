d4b8e2df82ddf7a15b0c18590189fc236c76567a
Marius Skjellerud <m_skjellerud@hotmail.com>
2023-12-04 11:28:11 +0100

    display data

--- met/src/App.tsx
+++ met/src/App.tsx
@@ -2,11 +2,22 @@ import './App.css'
 import { useState, useEffect } from 'react'
 
 function App() {
+  const [showPosts, setShowPosts] = useState<any>()
   const apiUrl = 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=60.10&lon=9.58'
+
+  // let displayData
   function pullJson() {
     fetch(apiUrl)
       .then((response) => response.json())
       .then((responseData) => {
+        console.log(typeof responseData)
+        setShowPosts(() => (
+          <div>
+            <p key={responseData.id}>{responseData.properties.timeseries[84].time}</p>
+            <p key={responseData.id}>{responseData.properties.timeseries[85].time}</p>
+            <p key={responseData.id}>{responseData.properties.timeseries[86].time}</p>
+          </div>
+        ))
         console.log(responseData)
       })
     // return
@@ -19,6 +30,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello, World</h1>
+      {showPosts}
     </div>
   )
 }
