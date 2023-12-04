23e4b358bce2ad5e1f01bae02b33cc701cbbb6f2
Marius Skjellerud <m_skjellerud@hotmail.com>
2023-12-04 10:23:28 +0100

    fetch api data

--- met/src/App.tsx
+++ met/src/App.tsx
@@ -1,6 +1,21 @@
 import './App.css'
+import { useState, useEffect } from 'react'
 
 function App() {
+  const apiUrl = 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=60.10&lon=9.58'
+  function pullJson() {
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log(responseData)
+      })
+    // return
+  }
+  // empty array, on first mount
+  useEffect(() => {
+    pullJson()
+  }, [])
+
   return (
     <div className="App">
       <h1>Hello, World</h1>
