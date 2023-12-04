56abc12992f73d6dacdc84701cca9475a9f0f3e7
Marius Skjellerud <m_skjellerud@hotmail.com>
2023-12-04 14:50:26 +0100

    added interface

--- met/src/App.tsx
+++ met/src/App.tsx
@@ -6,13 +6,18 @@ function App() {
   const apiUrl = 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=60.10&lon=9.58'
   const userAgent = 'm_skjellerud@hotmail.com'
 
-  // interface Api {
-  //   properties: object
-  //   timeseries: object
-  //   // timeseries: Array<{
-  //   //   time: string
-  //   // }>
-  // }
+  interface Api {
+    properties: {
+      timeseries: {
+        time: string
+        data: {
+          instant: {
+            details: { air_temperature: number }
+          }
+        }
+      }[]
+    }
+  }
 
   function pullJson() {
     fetch(apiUrl, {
@@ -21,31 +26,31 @@ function App() {
       },
     })
       .then((response) => response.json())
-      .then((responseData) => {
+      .then((responseData: Api) => {
         // console.log(typeof responseData)
         setShowPosts(() => (
           <div className="Data">
             <ul>
               <li>
-                {`${responseData.properties.timeseries.at(-3).time} : ${
-                  responseData.properties.timeseries.at(-3).data.instant.details.air_temperature
-                }`}
+                {`${responseData.properties.timeseries.at(-3)
+                  ?.time} : ${responseData.properties.timeseries.at(-3)?.data.instant.details
+                  .air_temperature}`}
               </li>
               <li>
-                {`${responseData.properties.timeseries.at(-2).time} : ${
-                  responseData.properties.timeseries.at(-2).data.instant.details.air_temperature
-                }`}
+                {`${responseData.properties.timeseries.at(-2)
+                  ?.time} : ${responseData.properties.timeseries.at(-2)?.data.instant.details
+                  .air_temperature}`}
               </li>
               <li>
-                {`${responseData.properties.timeseries.at(-1).time} : ${
-                  responseData.properties.timeseries.at(-1).data.instant.details.air_temperature
-                }`}
+                {`${responseData.properties.timeseries.at(-1)
+                  ?.time} : ${responseData.properties.timeseries.at(-1)?.data.instant.details
+                  .air_temperature}`}
               </li>
             </ul>
           </div>
         ))
-        // console.log(typeof responseData.properties.timeseries)
-        // console.log(responseData)
+        console.log(typeof responseData.properties.timeseries)
+        console.log(responseData)
       })
   }
   // empty array, on first mount
