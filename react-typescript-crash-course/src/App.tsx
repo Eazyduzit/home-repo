0caaecc92df8b750702a1c9a39d99e534c12535f
Marius Skjellerud <m_skjellerud@hotmail.com>
2023-12-12 12:44:29 +0100

    bootstrap and component

--- react-typescript-crash-course/src/App.tsx
+++ react-typescript-crash-course/src/App.tsx
@@ -2,11 +2,23 @@ import './App.css'
 
 function App() {
   return (
-    <>
-      <div className="App">
-        <h1>Hola, Mundo!</h1>
+    <div className="container mt-3">
+      <div className="grid">
+        <div className="row">
+          <div className="col">
+            <p className="h3 fw-bold text-success">Hola, Mundo!</p>
+            <p className="fst-italic">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore rerum laudantium
+              beatae eius ipsum nam placeat, laboriosam ut velit nobis quo! Magni quaerat ex neque
+              sed unde rem sint! Provident.
+            </p>
+            <button className="btn btn-success btn-sm">
+              <i className="fa fa-book" /> Read More
+            </button>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   )
 }
 
