2b0ebcc7b4b3f20b4fca71400f01c6eaa405c8ef
Marius Skjellerud <m_skjellerud@hotmail.com>
2023-12-14 11:09:11 +0100

    added state props to component

--- react-typescript-crash-course/src/components/Employee.tsx
+++ react-typescript-crash-course/src/components/Employee.tsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 interface EmployeeProps {}
-interface EmployeeState {}
+interface EmployeeState {
+  name: string
+  age: number
+  title: string
+}
 
 const Employee: React.FC<EmployeeProps> = () => {
-  return <h3>Employee Component</h3>
+  const [state, setState] = useState<EmployeeState>({
+    name: 'Marius',
+    age: 39,
+    title: 'Software Engineer',
+  })
+  return (
+    <>
+      <h3>Employee Component</h3>
+      <ul className="list-group">
+        <li className="list-group-item">Name: {state.name}</li>
+        <li className="list-group-item">Age: {state.age}</li>
+        <li className="list-group-item">Title: {state.title}</li>
+      </ul>
+    </>
+  )
 }
 
 export default Employee
