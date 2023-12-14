3e318b322f1188343c5a0f04754623e009d1f7c5
Marius Skjellerud <m_skjellerud@hotmail.com>
2023-12-14 11:36:12 +0100

    added state props to class component

--- react-typescript-crash-course/src/components/EmployeeClass.tsx
+++ react-typescript-crash-course/src/components/EmployeeClass.tsx
@@ -1,14 +1,33 @@
 import React from 'react'
 
 interface EmployeeProps {}
-interface EmployeeState {}
+interface EmployeeState {
+  name: string
+  age: number
+  title: string
+}
 
 export class EmployeeClass extends React.Component<EmployeeProps, EmployeeState> {
   constructor(props: EmployeeProps) {
     super(props)
+    this.state = {
+      name: 'Marius',
+      age: 39,
+      title: 'Software Engineer',
+    } as EmployeeState
   }
 
   render() {
-    return <h3>Employee Class</h3>
+    const { name, age, title } = this.state
+    return (
+      <>
+        <h3>Employee Class Component</h3>
+        <ul className="list-group">
+          <li className="list-group-item">Name: {name}</li>
+          <li className="list-group-item">Age: {age}</li>
+          <li className="list-group-item">Title: {title}</li>
+        </ul>
+      </>
+    )
   }
 }
