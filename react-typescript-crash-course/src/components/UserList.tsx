96be7dbc6695264a63c1e0b8e221f94cff81bfc4
Marius Skjellerud <m_skjellerud@hotmail.com>
2023-12-15 11:10:34 +0100

    added use state and users

--- react-typescript-crash-course/src/components/UserList.tsx
+++ react-typescript-crash-course/src/components/UserList.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { UserListUser } from '../models/UserListUser'
 
 interface UserListProps {}
@@ -7,6 +7,31 @@ interface UserListState {
 }
 
 const UserList: React.FC<UserListProps> = () => {
+  const [state, setState] = useState<UserListState>({
+    users: [
+      {
+        snum: 'AAA101',
+        name: 'Elon',
+        age: 45,
+        designation: 'CEO',
+        company: 'SpaceX',
+      },
+      {
+        snum: 'AAA102',
+        name: 'John',
+        age: 42,
+        designation: 'Sr. Software Engineer',
+        company: 'SpaceX',
+      },
+      {
+        snum: 'AAA103',
+        name: 'Marius',
+        age: 39,
+        designation: 'Software Engineer',
+        company: 'SpaceX',
+      },
+    ],
+  })
   return (
     <>
       <h3>UserList Component</h3>
