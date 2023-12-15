5f499d78ef39a569e1a181d66920fc979f051742
Marius Skjellerud <m_skjellerud@hotmail.com>
2023-12-15 13:26:02 +0100

    mapping and displaying data from data set

--- react-typescript-crash-course/src/components/AxiosList.tsx
+++ react-typescript-crash-course/src/components/AxiosList.tsx
@@ -36,9 +36,46 @@ const AxiosList: React.FC<AxiosListProps> = () => {
       })
   }, [])
 
+  const { loading, users, errorMsg } = state
+
   return (
     <>
       <h3>Axios List Component</h3>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <table className="table table-hover text-center table-striped">
+              <thead className="table-primary">
+                <tr>
+                  <th>S.Number</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Phone</th>
+                  <th>Company</th>
+                  <th>Website</th>
+                </tr>
+              </thead>
+              <tbody>
+                {users.length > 0 &&
+                  users.map((user) => {
+                    return (
+                      <>
+                        <tr key={user.id}>
+                          <td>{user.id}</td>
+                          <td>{user.name}</td>
+                          <td>{user.email}</td>
+                          <td>{user.phone}</td>
+                          <td>{user.company.name}</td>
+                          <td>{user.website}</td>
+                        </tr>
+                      </>
+                    )
+                  })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
