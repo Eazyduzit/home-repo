5ebb66eddb12b98e495196381d49f28e8259d505
Marius Skjellerud <m_skjellerud@hotmail.com>
2023-12-18 23:45:23 +0100

    separated image data into own file

--- react-typescript-crash-course/src/components/imageSlider/ImageSlider.tsx
+++ react-typescript-crash-course/src/components/imageSlider/ImageSlider.tsx
@@ -1,16 +1,9 @@
 import React, { useEffect, useState } from 'react'
+import { imagesArray } from './ImageData'
 
 interface ImageSliderProps {
   name: string
 }
-
-const imagesArray = [
-  'https://unsplash.com/photos/PBHwSmhyt9w/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MXx8MTkyMCUyMHglMjAxMDgwfGVufDB8fHx8MTcwMjkzNDMyMnww&force=true&w=640',
-  'https://unsplash.com/photos/W_29nYyiIpA/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8Mnx8MTkyMCUyMHglMjAxMDgwfGVufDB8fHx8MTcwMjkzNDMyMnww&force=true&w=640',
-  'https://unsplash.com/photos/YeBI31kK2_4/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8M3x8MTkyMCUyMHglMjAxMDgwfGVufDB8fHx8MTcwMjkzNDMyMnww&force=true&w=640',
-  'https://unsplash.com/photos/mBQIfKlvowM/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8NHx8MTkyMCUyMHglMjAxMDgwfGVufDB8fHx8MTcwMjkzNDMyMnww&force=true&w=640',
-  'https://unsplash.com/photos/RChZT-JlI9g/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8NXx8MTkyMCUyMHglMjAxMDgwfGVufDB8fHx8MTcwMjkzNDMyMnww&force=true&w=640',
-]
 
 const ImageSlider: React.FC<ImageSliderProps> = ({ name }) => {
   const [activeImageIndex, setActiveImageIndex] = useState(0)
