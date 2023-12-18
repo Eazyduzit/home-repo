8d0b8ae2759974834273bbe0407e757105b2092a
Marius Skjellerud <m_skjellerud@hotmail.com>
2023-12-18 22:37:05 +0100

    added image array and img tag

--- react-typescript-crash-course/src/components/imageSlider/ImageSlider.tsx
+++ react-typescript-crash-course/src/components/imageSlider/ImageSlider.tsx
@@ -4,11 +4,19 @@ interface ImageSliderProps {
   name: string
 }
 
+const imagesArray = [
+  'https://unsplash.com/photos/PBHwSmhyt9w/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8MXx8MTkyMCUyMHglMjAxMDgwfGVufDB8fHx8MTcwMjkzNDMyMnww&force=true&w=640',
+  'https://unsplash.com/photos/W_29nYyiIpA/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8Mnx8MTkyMCUyMHglMjAxMDgwfGVufDB8fHx8MTcwMjkzNDMyMnww&force=true&w=640',
+  'https://unsplash.com/photos/YeBI31kK2_4/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8M3x8MTkyMCUyMHglMjAxMDgwfGVufDB8fHx8MTcwMjkzNDMyMnww&force=true&w=640',
+  'https://unsplash.com/photos/mBQIfKlvowM/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8NHx8MTkyMCUyMHglMjAxMDgwfGVufDB8fHx8MTcwMjkzNDMyMnww&force=true&w=640',
+  'https://unsplash.com/photos/RChZT-JlI9g/download?ixid=M3wxMjA3fDB8MXxzZWFyY2h8NXx8MTkyMCUyMHglMjAxMDgwfGVufDB8fHx8MTcwMjkzNDMyMnww&force=true&w=640',
+]
+
 const ImageSlider: React.FC<ImageSliderProps> = ({ name }) => {
   return (
     <>
       <h3>{name}</h3>
-      <div></div>
+      <img src={imagesArray[0]} alt="Wallpaper image" />
     </>
   )
 }
